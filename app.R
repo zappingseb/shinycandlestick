ebf1a08aabdfa955990fc05ff2de00d07959364a
Wolf <wolfs25@emea.roche.com>
2019-02-24 14:39:32 +0100

    made candlestick work without being pre-loaded

--- app.R
+++ app.R
@@ -8,26 +8,25 @@ devtools::load_all(".")
 ui <- fluidPage(
   shinyjs::useShinyjs(),
   titlePanel("Custom input example"),
-  
-  fluidRow(
-    column(4, wellPanel(
-      CandleStick(id="myurl",candlestick_mode='option', candlestick_size="md"),
-      tags$br(),
-      CandleStick(id="mf",candlestick_mode='contents', candlestick_size="sm"),
-      tags$br(),
-      CandleStick(id="mfstick2",candlestick_mode='contents', candlestick_size="sm",
-                  left=c("f"="female"),
-                  right=c("m"="male"),
-                  default=c("b"="both")
-                  ,fa_mode=FALSE,
-                  contents_color = 'red')
-    )),
-    column(8, wellPanel(
-      verbatimTextOutput("urlText"),
-      verbatimTextOutput("mfout"),
-      verbatimTextOutput("mf2out")
-    ))
-  )
+    fluidRow(
+      column(4, wellPanel(
+        CandleStick(id="myurl",candlestick_mode='option', candlestick_size="md"),
+        tags$br(),
+        CandleStick(id="mf",candlestick_mode='contents', candlestick_size="md"),
+        tags$br(),
+        CandleStick(id="mfstick2",candlestick_mode='contents', candlestick_size="sm",
+                    left=c("f"="female"),
+                    right=c("m"="male"),
+                    default=c("b"="both")
+                    ,fa_mode=FALSE,
+                    contents_color = 'red')
+        )),
+        column(8, wellPanel(
+        verbatimTextOutput("urlText"),
+        verbatimTextOutput("mfout"),
+        verbatimTextOutput("mf2out")
+      ))
+    )
 )
 
 server <- function(input, output, session) {
@@ -41,7 +40,6 @@ server <- function(input, output, session) {
     output$mf2out <- renderText({
       as.character(input$mfstick2)
     })
-    
     
 }
 
